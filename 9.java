3976e4856a6b338de5fb6efab609919f17a27ba3
Shravani Korde <149994109+ShravaniKorde@users.noreply.github.com>
2025-10-04 19:07:40 +0530

    solution added

---
+++ 9.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+
+        int rev = 0;
+        int dup = Math.abs(x);
+
+        while(x!=0){
+            int ld = x%10;
+            rev = (rev * 10) + ld;
+            x = x/10;
+        }
+        if(dup == rev){
+            return true;
+        }
+        return false;
+        
+    }
+}
